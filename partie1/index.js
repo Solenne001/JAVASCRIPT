3521661d81535c15e4e20ca9e1a8d96f65c43ea7
solenne <tomsosso2@gmail.com>
2023-08-08 12:41:03 +0200

    essaie

---
+++ partie1/index.js
@@ -0,0 +1,27 @@
+/* let nom //declaration de variable
+nom ="Valentin" //affectation */
+/* //camelcase
+let nomEtudiant = "Reddington"
+
+const nombreDevie =100
+console.log(nom, nomEtudiant, nombreDevie)
+
+console.log(5) */
+
+/* let age =prompt("quelle est votre age?")
+console.log(age)
+
+let name =prompt("quelle est votre nom ?")
+let prenom =prompt("quelle est votre prenom ?")
+
+
+console.log("bienvenue" +prenom) */
+
+
+//condition
+let age = 17
+if(age <= 17){
+    console.log("oui c'est une mineur")
+} else{
+    console.log("tu peux aller")
+}
